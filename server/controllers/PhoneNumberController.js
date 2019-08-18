a7b772627846bb8fbb6ae27b13bacf453d8a445e
JCanaks <chiomimo15@gmail.com>
2019-08-18 15:57:41 +0100

    ISSUE-#8(sortnumbers): implement feature to sort numbers and find max and min
    - implement functionality to sort phone numbers and find the maximum nad minimum numbers
    
    [Finishes ISSUE #8]

--- server/controllers/PhoneNumberController.js
+++ server/controllers/PhoneNumberController.js
@@ -4,6 +4,9 @@ import {
   getPhoneNumberList,
   responseFormat,
   storeRandomPhoneNumbers,
+  getMaxAndMinNumbers,
+  sortAscending,
+  sortDescending,
 } from '../utils/helpers/PhoneNumberControllerHelper';
 
 class PhoneNumberController {
@@ -31,7 +34,7 @@ class PhoneNumberController {
       const { amount } = req.body;
 
       if (!amount || amount < 1) {
-        return res.status(200).json(
+        return res.status(400).json(
           responseFormat('No number(s) generated please specify an ammount greater than or equal to 1', []),
         );
       }
@@ -39,9 +42,47 @@ class PhoneNumberController {
       const generatedDetails = await generateRandomPhoneNumbers(amount);
       storeRandomPhoneNumbers(generatedDetails.phoneNumbers);
 
-      return res.status(200).json(
+      return res.status(201).json(
         responseFormat(`${generatedDetails.total} phone number(s) successfully generated`, generatedDetails),
       );
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async getSortedPhoneNumbers(req, res) {
+    try {
+      const phoneBook = {
+        total: 0,
+        min: '0',
+        max: '0',
+        phoneNumbers: [],
+      };
+      const readLineInstance = await generateReadlineInstance();
+
+      await getMaxAndMinNumbers(readLineInstance, phoneBook);
+
+      readLineInstance.on('close', () => {
+        if (phoneBook.phoneNumbers.length < 1) {
+          return res.status(200).json(
+            responseFormat('There is currently no phone numbers on the List', {
+              total: phoneBook.total,
+              phoneNumbers: phoneBook.phoneNumbers,
+            }),
+          );
+        }
+
+        const { order = '' } = req.query;
+        if (order.toLowerCase() === 'asc' || order.toLowerCase() === 'ascending') {
+          sortAscending(phoneBook.phoneNumbers);
+        }
+        if (order.toLowerCase() === 'desc' || order.toLowerCase() === 'descending') {
+          sortDescending(phoneBook.phoneNumbers);
+        }
+        return res.status(200).json(
+          responseFormat('Phone Numbers Fetched Successfully', phoneBook),
+        );
+      });
     } catch (error) {
       throw error;
     }
--- server/routes/phoneNumbers.js
+++ server/routes/phoneNumbers.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.get('/phonenumbers', PhoneNumberController.getPhoneNumbers);
 router.post('/phonenumbers', PhoneNumberController.generatePhoneNumbers);
+router.get('/sort', PhoneNumberController.getSortedPhoneNumbers);
 
 export default router;
